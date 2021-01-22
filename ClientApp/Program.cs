814d945680f9b79d9c50e9a0e725f8694fdf2540
Galdin Raphael <gldraphael@users.noreply.github.com>
2021-01-22 12:30:24 +0530

    Make it easier to test https://github.com/MassTransit/MassTransit/issues/2256

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -11,6 +11,21 @@ using System.Linq;
 using System.Threading.Tasks;
 
 
+async Task Run(WorkerService worker)
+{
+    // send 10 requests to the worker app
+    // and print the responses as they come in
+    var tasks = Enumerable.Range(1, 10)
+        .Select(id => 
+            worker.GetInfoAsync(id, timeout: TimeSpan.FromSeconds(7))
+                  .ContinueWith(r => {
+                    Log.Information("[{@id}]: {@Result}", id, r.Result);
+                  }
+            ));
+    await Task.WhenAll(tasks);
+}
+
+
 // setup configuration
 var config = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -48,14 +63,22 @@ await bus.StartAsync();
 
 try
 {
-    var tasks = Enumerable.Range(1, 10)
-        .Select(id => 
-            worker.GetInfoAsync(id, timeout: TimeSpan.FromSeconds(7))
-                  .ContinueWith(r => {
-                    Log.Information("[{@id}]: {@Result}", id, r.Result);
-                  }
-            ));
-    await Task.WhenAll(tasks);
+    /**
+     * Re: https://github.com/MassTransit/MassTransit/issues/2256
+     * There are two things to try:
+     * 1. Execute Run(), and then wait for long enough for the temporary queue to get auto-deleted, 
+     *    before executing Run() again.
+     *    I've tried this, but the temporary queue never got autodeleted.
+     * 2. Execute Run(), and then wait for a short period of time. Manually delete the temporary queue
+     *    during the wait time, before executing Run() again.
+     *    I'm able to reproduce #2256 with this approach. 
+     */
+
+    await Run(worker);
+    Log.Information("Delete the temporary queue now.");
+    await Task.Delay(TimeSpan.FromMinutes(1));
+    Log.Information("Continuing execution...");
+    await Run(worker);
 }
 finally
 {
