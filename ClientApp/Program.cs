8191a6b93864076f1bbc6438e384a8fed5666967
Galdin Raphael <gldraphael@users.noreply.github.com>
2021-01-25 20:07:49 +0530

    Update...

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -11,17 +11,21 @@ using System.Linq;
 using System.Threading.Tasks;
 
 
-async Task Run(WorkerService worker)
+WorkerService? worker;
+async Task Run(int numberOfRequests)
 {
-    // send 10 requests to the worker app
+    // send `numberOfRequests` requests to the worker app
     // and print the responses as they come in
-    var tasks = Enumerable.Range(1, 10)
-        .Select(id => 
-            worker.GetInfoAsync(id, timeout: TimeSpan.FromSeconds(7))
-                  .ContinueWith(r => {
+    var tasks = Enumerable.Range(1, numberOfRequests)
+        .Select(id => {
+
+            return worker.GetInfoAsync(id, timeout: TimeSpan.FromMinutes(2))
+                .ContinueWith(r =>
+                {
                     Log.Information("[{@id}]: {@Result}", id, r.Result);
-                  }
-            ));
+                }
+            );
+        });
     await Task.WhenAll(tasks);
 }
 
@@ -57,28 +61,19 @@ var serviceProvider = (new ServiceCollection() as IServiceCollection)
     
 
 
-var worker = serviceProvider.GetRequiredService<WorkerService>();
+worker = serviceProvider.GetRequiredService<WorkerService>();
 var bus = serviceProvider.GetRequiredService<IBusControl>();
 await bus.StartAsync();
 
 try
 {
-    /**
-     * Re: https://github.com/MassTransit/MassTransit/issues/2256
-     * There are two things to try:
-     * 1. Execute Run(), and then wait for long enough for the temporary queue to get auto-deleted, 
-     *    before executing Run() again.
-     *    I've tried this, but the temporary queue never got autodeleted.
-     * 2. Execute Run(), and then wait for a short period of time. Manually delete the temporary queue
-     *    during the wait time, before executing Run() again.
-     *    I'm able to reproduce #2256 with this approach. 
-     */
-
-    await Run(worker);
-    Log.Information("Delete the temporary queue now.");
+    await Run(numberOfRequests: 1);
+    Log.Information("Manually delete the temporary queue now.");
     await Task.Delay(TimeSpan.FromMinutes(1));
     Log.Information("Continuing execution...");
-    await Run(worker);
+    await Run(numberOfRequests: 1);
+    Log.Information("Re-running...");
+    await Run(numberOfRequests: 1);
 }
 finally
 {
